4d5e1e96f62d9887cf376c6b67fa80452661af3b
Moutloatsi Setlogelo <stlmou001@myuct.ac.za>
2021-09-17 21:44:35 +0200

    v1

--- Practical3/WorkPackage3/p3.py
+++ Practical3/WorkPackage3/p3.py
@@ -163,6 +163,7 @@ def btn_increase_pressed(channel):
     # Increase the value shown on the LEDs
     a=bin(guess).replace("0b","00")
     a=a[::-1]
+    print("Your guess" + guess)
     print(int(a[0]))
     print(int(a[1]))
     print(int(a[2]))
