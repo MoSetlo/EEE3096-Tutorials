2e95c5e52f55ee0f3adc7912d472de8a543797c1
Moutloatsi Setlogelo <stlmou001@myuct.ac.za>
2021-10-08 22:26:15 +0200

    New v3

--- Practical4/practical4.py
+++ Practical4/practical4.py
@@ -7,15 +7,20 @@ import RPi.GPIO as GPIO
 import threading
 import time
 
+# create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# create the cs (chip select)
 cs = digitalio.DigitalInOut(board.D5)
+
+# create the mcp object
 mcp = MCP.MCP3008(spi, cs)
 
 i=0
-sampling = 10
-arrSampling = [10, 5, 1]
+sampling = 10  #current sampling rate
+arrSampling = [10, 5, 1] #sampling rates
 
-
+#button interrupt configuration
 def btn_pressed(channel):
     global i, sampling
     if (GPIO.input(channel)==0):
@@ -28,12 +33,18 @@ def btn_pressed(channel):
 GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.add_event_detect(26, GPIO.FALLING, callback = btn_pressed, bouncetime = 200)
 
+
 def print_time_thread():
+    #sampling rate addition
     thread = threading.Timer(sampling, print_time_thread)
     thread.daemon = True
     thread.start()
+
+    # create an analog input channel on pin 1 for the temp sensor and pin 2 for LDR
     chan2 = AnalogIn(mcp, MCP.P2)
     chan1 = AnalogIn(mcp, MCP.P1)
+
+    #temperature calculation in degrees
     degree = round((chan1.voltage - 0.5) * 100,2)
     stop = time.time()
     
